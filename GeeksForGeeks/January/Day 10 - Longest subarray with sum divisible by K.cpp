98085d888373b97e3d89becdbdb13059038c3485
Ajay Rawat <theajayrawat@gmail.com>
2024-01-11 00:01:26 +0530

    day10

---
+++ GeeksForGeeks/January/Day 10 - Longest subarray with sum divisible by K.cpp
@@ -0,0 +1,30 @@
+// https://www.geeksforgeeks.org/problems/longest-subarray-with-sum-divisible-by-k1259/1
+// SC: O(N)
+// TC: O(N)
+
+class Solution{
+public:	
+	int longSubarrWthSumDivByK(int arr[], int n, int k)
+	{
+	    // Complete the function
+	   unordered_map<int,int>mp;
+	   int sum=0;
+	   int ans=0;
+	   for(int i=0;i<n;i++){
+	       sum+=arr[i];
+	       int rem=sum%k;
+	       if(rem<0) 
+	            rem +=k;
+	       if(rem == 0) 
+	            ans = i+1;
+	       else{
+	           if(mp.find(rem)!=mp.end())
+	                ans=max(ans,i-mp[rem]);
+	           else
+	                mp[rem]=i;
+	       }
+	       
+	   }
+	   return ans;
+	}
+};
---
+++ LeetCode/January/Day 10 - 2385. Amount of Time for Binary Tree to Be Infected.cpp
@@ -0,0 +1,80 @@
+// https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/
+// SC: O(N)
+// TC: O(N)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    stack<pair<TreeNode*,int>>st;
+    int min=0;
+    bool find=false;
+    void solve(TreeNode* root, int start){
+        if(!root || find)
+            return;
+        if(root->val==start){
+            st.push(make_pair(root,min++));
+            find=true;
+            return;
+        }
+
+        solve(root->left,start);
+        solve(root->right,start);
+        if(find) 
+            st.push(make_pair(root,min++));
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        if(!root || (!root->left && !root->right))
+            return 0;
+        solve(root,start);
+        int ans=min-1;
+        int size=st.size();
+        queue<pair<TreeNode*,int>>q;
+        while(size--){
+            auto i=st.top();
+            st.pop();
+            q.push(i);
+        }
+       
+        while(!q.empty()){
+            int qSize=q.size();
+            while(qSize--){
+                 pair<TreeNode*,int>p=q.front();
+            q.pop();
+            TreeNode* temp=p.first;
+            int time=p.second+1;
+            if(qSize!=0){
+                if(temp->left && temp->left!=q.front().first){
+                    q.push(make_pair(temp->left,time));
+                    ans=max(ans,time);
+                }if(temp->right && temp->right!=q.front().first){
+                    q.push(make_pair(temp->right,time));
+                     ans=max(ans,time);
+                }
+            }else{
+                if(temp->left){
+                    q.push(make_pair(temp->left,time));
+                     ans=max(ans,time);
+                }if(temp->right){
+                    q.push(make_pair(temp->right,time));
+                     ans=max(ans,time);
+                }
+            }
+
+            }
+            cout<<endl;
+           
+        }
+
+        return ans; 
+    }
+};

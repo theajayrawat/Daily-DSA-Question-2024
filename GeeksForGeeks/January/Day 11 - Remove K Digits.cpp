163c69159031067bf6b69766e4bb61b70de57bb5
Ajay Rawat <theajayrawat@gmail.com>
2024-01-11 23:51:50 +0530

    day11

---
+++ GeeksForGeeks/January/Day 11 - Remove K Digits.cpp
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/remove-k-digits/1
+// SC: O(N)
+// TC: O(N)
+
+class Solution {
+  public:
+    string removeKdigits(string S, int K) {
+        string ans="";
+        for(char ch:S){
+            while(ans.size() && ans.back()>ch  && K){
+                ans.pop_back();
+                K--;
+            }
+            if(ans.size() || ch!='0'){
+                ans.push_back(ch);
+            }
+        }
+        while(ans.size() && K){
+            ans.pop_back();
+            K--;
+            
+        }
+        if(ans==""){
+            return "0";
+        }
+        return ans;
+    }
+};
+
---
+++ LeetCode/January/Day 11 - 1026. Maximum Difference Between Node and Ancestor.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+// SC: O(N)
+// TC: O(N)
+
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        if(!root)
+            return 0;
+        int maxi=root->val,mini=root->val;
+        int diff=0;
+        solve(root,maxi,mini,diff);
+        return diff;
+    } 
+    void solve(TreeNode* root, int maxi, int mini, int &diff){
+        if(!root)
+            return;
+        diff=max(diff, max(abs(maxi-root->val),abs(root->val-mini)));
+        maxi=max(maxi,root->val);
+        mini=min(mini,root->val);
+
+        solve(root->left,maxi,mini,diff);
+        solve(root->right,maxi,mini,diff);
+    }   
+};

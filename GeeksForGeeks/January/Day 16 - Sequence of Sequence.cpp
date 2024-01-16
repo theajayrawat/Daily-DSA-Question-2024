94ea553856fcab293720c4d98850b9a2319e234d
Ajay Rawat <146063770+ajayrawatkeyideas@users.noreply.github.com>
2024-01-16 22:57:11 +0530

    day16

---
+++ GeeksForGeeks/January/Day 16 - Sequence of Sequence.cpp
@@ -0,0 +1,34 @@
+// https://www.geeksforgeeks.org/problems/sequence-of-sequence1155/1
+// O(M*N)
+// O(M*N)
+
+class Solution{
+public:
+    int dp[101][101];
+    int solve(int m, int n, int i, int cnt){
+        if(n==cnt)
+            return 1;
+        if(dp[i][cnt]!=-1) 
+            return dp[i][cnt];
+        
+        int ans=0;
+        for(int j=2*i;j<=m;j++){
+            ans+=solve(m,n,j,cnt+1);
+        }
+        
+        return dp[i][cnt]=ans;
+            
+    }
+    int numberSequence(int m, int n){
+        // code here
+        memset(dp,-1,sizeof(dp));
+        int ans=0;
+        for(int i=1;i<=m;++i)
+        {
+            //solve(val,ind,m,n)
+            ans+=solve(m,n,i,1);
+        }
+        return ans;
+        
+    }
+};
---
+++ LeetCode/January/Day 16 - 380. Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/insert-delete-getrandom-o1/
+// SC: O(1)
+// TC: O(1)
+
+class RandomizedSet {
+public:
+   unordered_map<int,int>mp;
+    vector<int>vec;
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(mp.find(val)!=mp.end())
+            return false;
+        vec.push_back(val);
+        mp[val] = vec.size()-1;
+        return true;
+    }
+    
+    bool remove(int val) {
+         if(mp.find(val)==mp.end())
+            return false;
+
+        int idx = mp[val];
+        int n=vec.size();
+        int temp=vec[n-1];
+        swap(vec[idx],vec[n-1]);
+        vec[idx]= temp;
+        mp[temp]= idx;
+        vec.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        int idx = rand()%vec.size();
+        return vec[idx];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */

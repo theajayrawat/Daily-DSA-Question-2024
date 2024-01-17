dabb05a94ea067424db33e2eb65adfd6eadaca7e
Ajay Rawat <146063770+ajayrawatkeyideas@users.noreply.github.com>
2024-01-17 22:32:24 +0530

    day17

---
+++ GeeksForGeeks/January/Day 17 - All Unique Permutations of an array.cpp
@@ -0,0 +1,35 @@
+// https://www.geeksforgeeks.org/problems/all-unique-permutations-of-an-array/1
+// O(N!)
+// O(N)
+
+class Solution {
+  public:
+    void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+        if (index == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        unordered_set<int>st;
+        for (int i = index; i < nums.size(); i++) {
+            if(st.find(nums[i])==st.end()){
+                swap(nums[index], nums[i]);
+                recurPermute(index + 1, nums, ans);
+                swap(nums[index], nums[i]);
+            }
+            st.insert(nums[i]); 
+      }
+    }
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        // code here
+        sort(arr.begin(),arr.end());
+         vector < vector < int >> ans;
+        recurPermute(0, arr, ans);
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
+
+
+
---
+++ LeetCode/January/Day 17 - 207. Unique Number of Occurrences.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/unique-number-of-occurrences/
+// SC: O(N)
+// TC: O(N)
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> m;
+        for(int i=0; i<arr.size(); i++){
+            m[arr[i]]++;
+        }
+        unordered_set<int> s;
+        for(auto x : m ){
+            int freq = x.second;
+            if(s.find(freq) != s.end()) return false;
+            else{
+                s.insert(freq);
+            }
+        }
+        return true;
+    }
+};
+

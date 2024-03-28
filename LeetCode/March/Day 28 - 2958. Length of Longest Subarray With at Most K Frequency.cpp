8e33a3c7ab92f33fef6548c9f8d1265bbc20d5c8
Ajay Rawat <theajayrawat@gmail.com>
2024-03-28 22:28:05 +0530

    day 28

---
+++ LeetCode/March/Day 28 - 2958. Length of Longest Subarray With at Most K Frequency.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/
+// TC: O(N)
+// SC: O(N)
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int ans = 0, start = -1;
+        unordered_map<int, int> frequency;
+        
+        for (int end = 0; end < nums.size(); end++) {
+            frequency[nums[end]]++;
+            while (frequency[nums[end]] > k) {
+                start++;
+                frequency[nums[start]]--;
+            }
+            ans = max(ans, end - start);
+        }
+        
+        return ans;
+    }
+};
